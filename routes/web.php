d357e2d7cb861d93e02ea0b7fee1e5a58fdf6364
Vishal Kumar <vishalgupta80906@gmail.com>
2023-09-07 18:08:17 +0530

    vishal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout/home');
 });
+
+Route::get('admin',[AdminController::class,'index']);
+Route::post('admin/auth',[AdminController::class,'auth'])->name('admin.auth');
+
+
+Route::group(['middleware'=>'admin_auth'],function(){
+    Route::get('admin/dashboard',[AdminController::class,'dashboard']);
+    Route::get('admin/category',[CategoryController::class,'index']);
+});
+
+
+
